a54eacf84e0fa88f0e45ff1aa4ca44d80cc9396d
wendy <wendy.wang@cudatec.com>
2016-01-07 13:48:15 +0800

    create

---
+++ TestLogin/src/TestServlet.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 设置响应内容类型
+		response.setContentType("text/html");
+
+		String ver = request.getParameter("code");
+		System.out.println(ver);
+	}
+
+}
